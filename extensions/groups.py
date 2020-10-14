5a6f5af05d733fa62fbfa5cb2c7a6eb56d083159
Titus Kirch <requests@tkirch.dev>
2020-10-14 17:17:14 +0200

    feat: improve groupphase commands

--- extensions/groups.py
+++ extensions/groups.py
@@ -6,6 +6,7 @@ from discord.ext import commands
 from db import db_session, db_engine, Session
 
 from models.state import State
+from models.groupphaseuser import Groupphaseuser
 
 
 class Groups(commands.Cog, name='Groups'):
@@ -17,6 +18,12 @@ class Groups(commands.Cog, name='Groups'):
         if isinstance(self.guild_announcements_channel_id, str):
             self.guild_announcements_channel_id = int(
                 self.guild_announcements_channel_id)
+
+        self.bot_user_id = os.getenv(
+            'BOT_USER_ID')
+        if isinstance(self.bot_user_id, str):
+            self.bot_user_id = int(
+                self.bot_user_id)
 
     @commands.command(aliases=['gruppenphase'], hidden=True)
     @commands.has_permissions(administrator=True)
@@ -92,6 +99,7 @@ class Groups(commands.Cog, name='Groups'):
                 # delete groupphase
                 State.delete('groupphase_isStarted')
                 State.delete('groupphase_reaction_message')
+                Groupphaseuser.deleteall()
 
                 # create output embed
                 embed = discord.Embed(
@@ -112,15 +120,85 @@ class Groups(commands.Cog, name='Groups'):
 
                 # send embed
                 await ctx.send(ctx.author.mention, embed=embed)
+
+        elif type == 'info':
+            # check if grouphase is startet
+            state_groupphase_isStarted = State.get('groupphase_isStarted')
+            if (state_groupphase_isStarted and state_groupphase_isStarted.value == str(True)):
+
+                # create output embed
+                embed = discord.Embed(
+                    colour=discord.Colour.blue(),
+                    title="Informationen zur Gruppenphase",
+                )
+
+                members_total = len(Groupphaseuser.all())
+
+                embed.add_field(name='Teilnehmer',
+                                value=f'{members_total}', inline=False)
+
+                # send message
+                await ctx.send(ctx.author.mention, embed=embed)
+
+            else:
+
+                # create output embed
+                embed = discord.Embed(
+                    colour=discord.Colour.red(),
+                    title=f'Es läuft keine Gruppenphase, über die du Informationen anfodern kannst!'
+                )
+
+                # send embed
+                await ctx.send(ctx.author.mention, embed=embed)
         else:
             # create output embed
             embed = discord.Embed(
                 colour=discord.Colour.red(),
-                title=f'Bitte übergebe `start` oder `stop` als erstes Argument.'
+                title=f'Bitte übergebe `start`, `stop` oder `info` als erstes Argument.'
             )
 
             # send embed
             await ctx.send(ctx.author.mention, embed=embed)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_add(self, payload):
+        # check if bot react
+        if payload.user_id == self.bot_user_id:
+            return
+
+        # check if grouphase is startet
+        state_groupphase_isStarted = State.get('groupphase_isStarted')
+        if (state_groupphase_isStarted and state_groupphase_isStarted.value == str(True)):
+            state_groupphase_reaction_message = State.get(
+                'groupphase_reaction_message')
+            if (state_groupphase_reaction_message and int(state_groupphase_reaction_message.value) == payload.message_id):
+
+                # check emoji
+                if payload.emoji.name == '✅':
+                    Groupphaseuser.set(payload.user_id)
+                else:
+                    channel = self.bot.get_channel(payload.channel_id)
+                    message = await channel.fetch_message(payload.message_id)
+                    user = discord.Member
+                    user.id = payload.user_id
+                    await message.remove_reaction(payload.emoji, user)
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        # check if bot react
+        if payload.user_id == self.bot_user_id:
+            return
+
+        # check if grouphase is startet
+        state_groupphase_isStarted = State.get('groupphase_isStarted')
+        if (state_groupphase_isStarted and state_groupphase_isStarted.value == str(True)):
+            state_groupphase_reaction_message = State.get(
+                'groupphase_reaction_message')
+            if (state_groupphase_reaction_message and int(state_groupphase_reaction_message.value) == payload.message_id):
+
+                # check emoji
+                if payload.emoji.name == '✅':
+                    Groupphaseuser.delete(payload.user_id)
 
 
 def setup(bot):
