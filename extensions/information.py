5874e2dbd48eebc40dc3a857799ec0c925b60bc2
Patrik Schmolke <patrik.schmolke@alumni.fh-aachen.de>
2020-10-22 11:20:13 +0200

    inital commit for information extension

---
+++ extensions/information.py
@@ -0,0 +1,14 @@
+import discord
+import os
+import typing
+
+class Information(commands.Cog, name='Informations'):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['services'], hidden=True)
+    async def shortMentionForServices(self, ctx, active: typing.Optional[str]):
+
+
+def setup(bot):
+    bot.add_cog(ManualGroups(bot))
