600aa567de6b51d40ea8a67fd194f590726c30c4
Patrik Schmolke <patrik.schmolke@alumni.fh-aachen.de>
2020-10-22 15:46:02 +0200

    further information requests

--- extensions/information.py
+++ extensions/information.py
@@ -10,7 +10,8 @@ class Information(commands.Cog, name='Informations'):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['services'])
+    @commands.command(aliases=['services','dienste'])
+    @commands.cooldown(1, 60, commands.BucketType.user)
     async def shortMentionForServices( self, ctx ):
         time = datetime.fromisoformat('2020-10-22')
         embed = discord.Embed(
@@ -18,25 +19,25 @@ class Information(commands.Cog, name='Informations'):
             title = 'Verfügbare Services der FH Aachen',
             description = 'Für weitere Infos kannst du den Befehl zum entsprechenden Service benutzen',
             url = 'https://www.fh-aachen.de/fachbereiche/elektrotechnik-und-informationstechnik/',
-            timestamp = time
+            timestamp = time,
+            thumbnail = '../resources/logo-FB5.jpg'
         )
-        embed.set_thumbnail(url='https://www.fh-aachen.de/fileadmin/template/pics/logo_alt/logo-FH.png')
         embed.set_footer( text='Alle Angaben ohne Gewähr!', 
-                        icon_url='' )
+                        icon_url='../resources/logo-FB5.jpg' )
         embed.add_field(
             name = 'Services',
-            value = 'Allgemeine FH-Account Verwaltung.\nhttps://services.fh-aachen.de/',
+            value = 'Allgemeine FH-Account Verwaltung.\nhttps://www.services.fh-aachen.de/',
             inline = False
         )
         embed.add_field(
             name = 'Ilias',
-            value = 'Die Online Lernplattform, wo alle Vorlesungsinhalte zusammengetragen sind.\nhttps://ili.fh-aachen.de/\n' +
+            value = 'Die Online Lernplattform, wo alle Vorlesungsinhalte zusammengetragen sind.\nhttps://www.ili.fh-aachen.de/\n' +
                     'Diese Seite bekommt dir hoffentlich bereits bekannt vor :)\nhttps://www.ili.fh-aachen.de/goto_elearning_crs_610475.html',
             inline = False
         )
         embed.add_field(
             name = 'eLectures',
-            value = 'Hier findest du Aufzeichnungen zu Vorlesungen (Meist aus Vor-Corona zeiten).\nhttps://www.electures.fh-aachen.de/',
+            value = 'Hier findest du Aufzeichnungen zu Vorlesungen \n(Meist aus Vor-Corona Zeiten).\nhttps://www.electures.fh-aachen.de/',
             inline = False
         )
         embed.add_field(
@@ -52,6 +53,57 @@ class Information(commands.Cog, name='Informations'):
         
 
         await ctx.send( ctx.author.mention, embed=embed )
+
+    @commands.command(aliases=['links, href'])
+    @commands.cooldown(1, 60, commands.BucketType.user)
+    async def listOfImportantUrls( self, ctx ):
+        time = datetime.fromisoformat('2020-10-22')
+        embed = discord.Embed(
+            colour = 0x00b5ad,
+            title = 'Wichtige Seiten',
+            description = 'Für weitere Infos kannst du den Befehl zum entsprechenden Service benutzen',
+            timestamp = time,
+            thumbnail = '../resources/logo-FB5.jpg'
+        )
+        embed.set_footer( text='Alle Angaben ohne Gewähr!', 
+                        icon_url='../resources/logo-FB5.jpg' )
+        embed.add_field(
+            name = 'Homepage',
+            value = 'http://www.fh-aachen.de/',
+            inline = False
+        )
+        embed.add_field(
+            name = 'FB5 - Fachbereich 5',
+            value = 'https://www.fh-aachen.de/fachbereiche/elektrotechnik-und-informationstechnik/',
+            inline = False
+        )
+        embed.add_field(
+            name = 'FSR - Fachschaftsrat',
+            value = 'https://fsr5.de/',
+            inline = False
+        )
+        embed.add_field(
+            name = 'AStA - Allgeminer Studierenden Ausschuss',
+            value = 'https://asta.fh-aachen.org/',
+            inline = False
+        )
+        embed.add_field(
+            name = 'ESP - ErstSemesterProjekt',
+            value = 'https://esp.fh-aachen.org/',
+            inline = False
+        )
+        embed.add_field(
+            name = 'Prüfungsamt',
+            value = 'https://www.fh-aachen.de/fachbereiche/elektrotechnik-und-informationstechnik/direkteinstieg/fuer-studierende/klausuren-pruefungen-pos/pruefungsamt/',
+            inline = False
+        )
+        embed.add_field(
+            name = 'DVZ - Datenverarbeitungszentrale',
+            value = 'https://www.fh-aachen.de/hochschule/datenverarbeitungszentrale/',
+            inline = False
+        )
+
+        await ctx.send(ctx.author.mention, embed=embed)
 
 
 def setup(bot):
